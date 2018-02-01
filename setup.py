48126ed4662b63f8f0b6b4f520f802708bfded89
Luke Benstead <kazade@gmail.com>
2018-02-01 09:44:24 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+
+NAME = 'simulant-tools'
+PACKAGES = find_packages()
+DESCRIPTION = 'Tools to harness the Simulant Game Engine'
+URL = "https://simulant-engine.appspot.com/"
+LONG_DESCRIPTION = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+AUTHOR = 'Luke Benstead'
+VERSION = '0.1-alpha'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=PACKAGES,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    keywords=["gamedev", "game", "engine", "simulant"],
+    url=URL,
+    classifiers=[
+        'Environment :: Console'
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Build Tools',
+    ],
+
+    include_package_data=True,
+)
